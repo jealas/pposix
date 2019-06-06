77794e59b84d49d50f9ea018ef6ed378a9752348
Jesse Alas <jessealas94@gmail.com>
2019-06-06 11:41:04 -0700

    Add more socket enums and create socket capi types.

---
+++ include/pposix/capi/rawfd_t.hpp
@@ -0,0 +1,10 @@
+#ifndef PPOSIX_RAWFD_T_HPP
+#define PPOSIX_RAWFD_T_HPP
+
+namespace pposix::capi {
+
+using rawfd_t = int;
+
+}
+
+#endif  // PPOSIX_RAWFD_HPP
---
+++ include/pposix/capi/socketfd.hpp
@@ -0,0 +1,13 @@
+#ifndef PPOSIX_CAPI_SOCKETFD_HPP
+#define PPOSIX_CAPI_SOCKETFD_HPP
+
+#include "pposix/capi/rawfd_t.hpp"
+
+
+namespace pposix::capi {
+
+    enum class socketfd : rawfd_t {};
+
+}
+
+#endif //PPOSIX_CAPI_SOCKETFD_HPP
