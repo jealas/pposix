95f58827f066ba02a78a3c843485b78c02a7de34
Jesse Alas <jessealas94@gmail.com>
2019-06-07 06:45:45 -0700

    Clean up rawfd implementation.

--- include/pposix/capi/rawfd.hpp
+++ include/pposix/capi/rawfd.hpp
@@ -6,13 +6,17 @@ namespace pposix::capi {
 
 using fd_t = int;
 
-class rawfd {
-  static constexpr fd_t NULLFD = -1;
+namespace detail {
 
+static constexpr fd_t NULLFD = -1;
+
+}
+
+class rawfd {
  public:
   rawfd() = default;
   constexpr explicit rawfd(fd_t fd) noexcept : fd_{fd} {}
-  constexpr explicit rawfd(nullfd_t) noexcept : fd_{NULLFD} {}
+  constexpr explicit rawfd(nullfd_t) noexcept : fd_{detail::NULLFD} {}
 
   rawfd(const rawfd &) = default;
   rawfd(rawfd &&) = default;
@@ -21,20 +25,20 @@ class rawfd {
   rawfd &operator=(rawfd &&) = default;
 
   constexpr rawfd &operator=(nullfd_t) noexcept {
-    fd_ = NULLFD;
+    fd_ = detail::NULLFD;
     return *this;
   }
 
   constexpr fd_t fd() const noexcept { return fd_; }
 
  private:
-  int fd_{NULLFD};
+  int fd_{detail::NULLFD};
 };
 
 constexpr bool operator==(rawfd lhs, rawfd rhs) { return lhs.fd() == rhs.fd(); }
 constexpr bool operator!=(rawfd lhs, rawfd rhs) { return lhs.fd() != rhs.fd(); }
 
-constexpr bool operator==(rawfd fd, nullfd_t) noexcept { return fd.fd() == fd_t{-1}; }
+constexpr bool operator==(rawfd fd, nullfd_t) noexcept { return fd.fd() == detail::NULLFD; }
 constexpr bool operator==(nullfd_t, rawfd fd) noexcept { return fd == nullfd; }
 
 constexpr bool operator!=(rawfd fd, nullfd_t) noexcept { return !(fd == nullfd); }
