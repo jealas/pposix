2358fb3350329763489f672c3678d59e1ffd3e7c
Jesse Alas <jessealas94@gmail.com>
2019-06-08 20:24:11 -0700

    Rename seek_whence to whence.

--- include/pposix/capi/file/lseek.hpp
+++ include/pposix/capi/file/lseek.hpp
@@ -2,16 +2,16 @@
 
 #include <unistd.h>
 
+#include "pposix/capi/file/offset.hpp"
+#include "pposix/capi/file/whence.hpp"
 #include "pposix/capi/rawfd.hpp"
 #include "pposix/errno_code.hpp"
-#include "pposix/capi/file/offset.hpp"
-#include "pposix/capi/file/seek_whence.hpp"
 #include "pposix/util/underlying_value.hpp"
 
 namespace pposix::capi::file {
 
-offset_t lseek(rawfd fd, offset_t offset, seek_whence whence) noexcept {
-  return ::lseek(fd.fd(), offset, util::underlying_value(whence));
+offset_t lseek(rawfd fd, offset_t offset, whence wh) noexcept {
+  return ::lseek(fd.fd(), offset, util::underlying_value(wh));
 }
 
 }  // namespace pposix::capi::file
