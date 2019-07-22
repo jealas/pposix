d6383f44f34cf403630dcc72fb09d432bcc75d4a
Jesse Alas <jessealas94@gmail.com>
2019-07-22 06:45:52 -0700

    Move files out of capi directory.

--- include/pposix/file/permission.hpp
+++ include/pposix/file/permission.hpp
@@ -4,6 +4,39 @@
 
 #include "pposix/capi/file/permission.hpp"
 
+namespace pposix::capi::file {
+
+    enum class permission : unsigned {
+        none = 0u,
+
+        user_read = S_IRUSR,
+        user_write = S_IWUSR,
+        user_execute = S_IXUSR,
+        user_all = S_IRWXU,
+
+        group_read = S_IRGRP,
+        group_write = S_IWGRP,
+        group_execute = S_IXGRP,
+        group_all = S_IRWXG,
+
+        other_read = S_IROTH,
+        other_write = S_IWOTH,
+        other_execute = S_IXOTH,
+        other_all = S_IRWXO,
+    };
+
+    constexpr permission operator|(permission lhs, permission rhs) noexcept {
+        return permission{util::underlying_value(lhs) | util::underlying_value(rhs)};
+    }
+
+    constexpr permission &operator|=(permission &lhs, permission rhs) noexcept {
+        lhs = lhs | rhs;
+        return lhs;
+    }
+
+
+}  // namespace pposix::capi::file
+
 namespace pposix::file::literals::permission_literals {
 
 namespace detail {
--- include/pposix/util/underlying_value.hpp
+++ include/pposix/util/underlying_value.hpp
@@ -3,18 +3,17 @@
 
 #include <type_traits>
 
-
 namespace pposix::util {
 
-    template<class Enum>
-    constexpr decltype(auto) underlying_value(const Enum e) {
-        using enum_t = std::decay_t<Enum>;
+template <class Enum>
+constexpr decltype(auto) underlying_value(const Enum e) {
+  using enum_t = std::decay_t<Enum>;
 
-        static_assert(std::is_enum_v<enum_t>);
+  static_assert(std::is_enum_v<enum_t>);
 
-        return static_cast<std::underlying_type_t<enum_t>>(e);
-    }
-
+  return static_cast<std::underlying_type_t<enum_t>>(e);
 }
 
-#endif //PPOSIX_UTIL_UNDERLYING_VALUE_HPP
+}  // namespace pposix::util
+
+#endif  // PPOSIX_UTIL_UNDERLYING_VALUE_HPP
