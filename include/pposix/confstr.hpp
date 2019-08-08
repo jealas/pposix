566337ea813f88f48d8187f94e1e2b8292fc0320
Jesse Alas <jessealas94@gmail.com>
2019-08-07 21:11:46 -0700

    Fixed macOS missing confstr names.

--- include/pposix/confstr.hpp
+++ include/pposix/confstr.hpp
@@ -5,13 +5,15 @@
 #include <unistd.h>
 
 #include "pposix/char_span.hpp"
-#include "pposix/result.hpp"
 #include "pposix/platform.hpp"
+#include "pposix/result.hpp"
 
 namespace pposix {
 
 enum class confstr_name : int {
   path = _CS_PATH,
+
+#if !PPOSIX_PLATFORM_MAC_OS
   v7_ilp32_off32_cflags = _CS_POSIX_V7_ILP32_OFF32_CFLAGS,
   v7_ilp32_off32_ldflags = _CS_POSIX_V7_ILP32_OFF32_LDFLAGS,
   v7_ilp32_off32_libs = _CS_POSIX_V7_ILP32_OFF32_LIBS,
@@ -24,14 +26,18 @@ enum class confstr_name : int {
   v7_lpbig_offbig_cflags = _CS_POSIX_V7_LPBIG_OFFBIG_CFLAGS,
   v7_lpbig_offbig_ldflags = _CS_POSIX_V7_LPBIG_OFFBIG_LDFLAGS,
   v7_lpbig_offbig_libs = _CS_POSIX_V7_LPBIG_OFFBIG_LIBS,
+#endif
 
-#if !PPOSIX_PLATFORM_LINUX
+#if !PPOSIX_PLATFORM_LINUX && !PPOSIX_PLATFORM_MAC_OS
   v7_threads_cflags = _CS_POSIX_V7_THREADS_CFLAGS,
   v7_threads_ldflags = _CS_POSIX_V7_THREADS_LDFLAGS,
 #endif
 
+#if !PPOSIX_PLATFORM_MAC_OS
   v7_width_restricted_envs = _CS_POSIX_V7_WIDTH_RESTRICTED_ENVS,
   v7_env = _CS_V7_ENV,
+#endif
+
   v6_ilp32_off32_cflags = _CS_POSIX_V6_ILP32_OFF32_CFLAGS,
   v6_ilp32_off32_ldflags = _CS_POSIX_V6_ILP32_OFF32_LDFLAGS,
   v6_ilp32_off32_libs = _CS_POSIX_V6_ILP32_OFF32_LIBS,
@@ -45,7 +51,10 @@ enum class confstr_name : int {
   v6_lpbig_offbig_ldflags = _CS_POSIX_V6_LPBIG_OFFBIG_LDFLAGS,
   v6_lpbig_offbig_libs = _CS_POSIX_V6_LPBIG_OFFBIG_LIBS,
   v6_width_restricted_envs = _CS_POSIX_V6_WIDTH_RESTRICTED_ENVS,
+
+#if !PPOSIX_PLATFORM_MAC_OS
   v6_env = _CS_V6_ENV
+#endif
 };
 
 result<size_t> confstr_length(confstr_name name) noexcept;
