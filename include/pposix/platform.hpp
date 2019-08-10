94e949172a1684f7a1fc8816ae4a5cda3cef4e2a
Jesse Alas <jessealas94@gmail.com>
2019-08-09 18:49:00 -0700

    Remove android platform.

--- include/pposix/platform.hpp
+++ include/pposix/platform.hpp
@@ -6,10 +6,9 @@ namespace pposix {
 #define PPOSIX_PLATFORM_MAC_OS 0
 #define PPOSIX_PLATFORM_FREE_BSD 0
 #define PPOSIX_PLATFORM_SOLARIS 0
-#define PPOSIX_PLATFORM_ANDROID 0
 #define PPOSIX_PLATFORM_LINUX 0
 
-enum class platform { mac_os, free_bsd, solaris, linux_, android };
+enum class platform { mac_os, free_bsd, solaris, linux_ };
 
 #if defined(__APPLE__) && __APPLE__ && defined(__MACH__) && __MACH__
 inline constexpr platform compilation_platform{platform::mac_os};
@@ -26,12 +25,6 @@ inline constexpr platform compilation_platform{platform::free_bsd};
 inline constexpr platform compilation_platform{platform::solaris};
 #undef PPOSIX_PLATFORM_SOLARIS
 #define PPOSIX_PLATFORM_SOLARIS 1
-
-#elif defined(__ANDROID__) && __ANDROID__
-inline constexpr platform compilation_platform{platform::android};
-
-#undef PPOSIX_PLATFORM_ANDROID
-#define PPOSIX_PLATFORM_ANDROID 1
 
 #elif defined(__linux__) && __linux__
 inline constexpr platform compilation_platform{platform::linux_};
