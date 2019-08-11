840ba988dddd7ad8586e5f77dd348f6c50f4d3dd
Jesse Alas <jessealas94@gmail.com>
2019-08-10 17:36:40 -0700

    Put capi functions in the correct namespace.

--- include/pposix/rt/mqueue.hpp
+++ include/pposix/rt/mqueue.hpp
@@ -9,11 +9,11 @@
 #include "pposix/unique_descriptor.hpp"
 #include "pposix/util.hpp"
 
-namespace pposix {
+namespace pposix::rt {
 
 enum class mqd_t : ::mqd_t { null = -1 };
 
-using mq_d = descriptor<pposix::mqd_t, pposix::mqd_t::null>;
+using mq_d = descriptor<rt::mqd_t, rt::mqd_t::null>;
 
 enum class mq_mode : int { read_only = O_RDONLY, write_only = O_WRONLY, read_write = O_RDWR };
 
@@ -23,11 +23,16 @@ struct mq_close_policy {
   std::error_code operator()(mq_d mq_descriptor) const noexcept;
 };
 
+class mq_attr;
+
+result<rt::mq_attr> mq_getattr(rt::mq_d mq_descriptor) noexcept;
+
+namespace capi {
+
 result<unique_descriptor<mq_d, mq_close_policy>> mq_open(const char* name, mq_mode mode,
                                                          mq_option option) noexcept;
 
-result<unique_descriptor<mq_d, mq_close_policy>> mq_open(const std::string& name, mq_mode mode,
-                                                         mq_option option) noexcept;
+}  // namespace capi
 
 /*
 The <mqueue.h> header shall define the mqd_t type, which is used for message queue descriptors.
@@ -61,11 +66,9 @@ Inclusion of the <mqueue.h> header may make visible symbols defined in the heade
  */
 
 // Forward declarations
-class mq_attr;
-result<mq_attr> mq_getattr(pposix::mq_d mq_descriptor) noexcept;
 
 class mq_attr {
-  constexpr long current_queued_message_count() const noexcept { return attributes_.mq_curmsgs; }
+  constexpr long current_message_count() const noexcept { return attributes_.mq_curmsgs; }
   constexpr long max_message_count() const noexcept { return attributes_.mq_maxmsg; }
   constexpr long max_message_size() const noexcept { return attributes_.mq_msgsize; }
 
@@ -77,7 +80,7 @@ class mq_attr {
     return attributes_.mq_flags & underlying_value(option);
   }
 
-  friend result<mq_attr> mq_getattr(pposix::mq_d mq_descriptor) noexcept;
+  friend result<rt::mq_attr> rt::mq_getattr(rt::mq_d mq_descriptor) noexcept;
 
  private:
   constexpr mq_attr(::mq_attr attributes) noexcept : attributes_{attributes} {}
@@ -85,6 +88,6 @@ class mq_attr {
   ::mq_attr attributes_{};
 };
 
-result<mq_attr> mq_getattr(pposix::mq_d mq_descriptor) noexcept;
+result<rt::mq_attr> mq_getattr(rt::mq_d mq_descriptor) noexcept;
 
-}  // namespace pposix
+}  // namespace pposix::rt
--- src/rt/mqueue.cpp
+++ src/rt/mqueue.cpp
@@ -2,7 +2,7 @@
 
 #include "pposix/errno.hpp"
 
-namespace pposix {
+namespace pposix::rt {
 
 std::error_code mq_close_policy::operator()(mq_d mq_descriptor) const noexcept {
   if (const int res{::mq_close(underlying_value(mq_descriptor.raw()))}; res == -1) {
@@ -12,29 +12,28 @@ std::error_code mq_close_policy::operator()(mq_d mq_descriptor) const noexcept {
   }
 }
 
+namespace capi {
+
 result<unique_descriptor<mq_d, mq_close_policy>> mq_open(const char* name, mq_mode mode,
                                                          mq_option option) noexcept {
-  pposix::mqd_t mq_descriptor{::mq_open(name, underlying_value(mode) | underlying_value(option))};
-  if (mq_descriptor == pposix::mqd_t::null) {
+  rt::mqd_t mq_descriptor{::mq_open(name, underlying_value(mode) | underlying_value(option))};
+  if (mq_descriptor == rt::mqd_t::null) {
     return current_errno_code();
   } else {
     return unique_descriptor<mq_d, mq_close_policy>{mq_d{mq_descriptor}};
   }
 }
 
-result<unique_descriptor<mq_d, mq_close_policy>> mq_open(const std::string& name, mq_mode mode,
-                                                         mq_option option) noexcept {
-  return pposix::mq_open(name.c_str(), mode, option);
-}
+}  // namespace capi
 
-result<mq_attr> mq_getattr(pposix::mq_d mq_descriptor) noexcept {
+result<rt::mq_attr> mq_getattr(rt::mq_d mq_descriptor) noexcept {
   ::mq_attr attributes{};
 
   const int result{::mq_getattr(underlying_value(mq_descriptor.raw()), &attributes)};
   if (result == -1) {
     return current_errno_code();
   } else {
-    return mq_attr{attributes};
+    return rt::mq_attr{attributes};
   }
 }
 
